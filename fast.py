9fca3046a53ea832d38a88b2eba3bae936588fab
Apiquet <anthony.piquet2@gmail.com>
2021-10-01 11:53:17 +0200

    [DEV][FAST] Homogenous distribution of kp

--- fast.py
+++ fast.py
@@ -43,6 +43,106 @@ def get_pixel_value(array, pixel_position):
     """
     return array[pixel_position[0], pixel_position[1]]
 
+def extract_patches(array, rows, cols):
+    """
+    Divide a 2D array into patches of size [rows*cols]
+
+    Args:
+        - (np.array) input array to divide
+        - (int) number of rows in a patch
+        - (int) number of cols in a patch
+    Return:
+        - (np.array) array of size [nb patch, rows, cols]
+    """
+    patches = array.reshape(
+        array.shape[0]//rows, rows, -1, cols).swapaxes(1,2).reshape(
+            -1, rows, cols)
+    return patches
+
+
+def detect_with_adaptive_threshold_and_grid(
+        img, nb_keypoints, N=12, step=5, epsilon=50,
+        percentage=0.1, init_thresholds=None, cols=4, rows=3):
+    """
+    Function to detect keypoints with adaptive threshold on multiple cells
+
+    Args:
+        - (np.array) input image
+        - (int) number of wanted keypoints
+        - (int) min number of neighbor to validate a pixel
+        - (int) step to do 1/step pixels
+            (if step=1: computation done on every pixel)
+        - (int) epsilon to accept a number of keypoints
+        - (float) percentage to change the threshold per iteration
+        - (int) value to initialize the threshold (default is 15)
+        - (int) number of columns
+        - (int) number of rows
+    Return:
+        - (np.array) vector of detected keypoints
+            [Number of keypoints, x, y]
+    """
+    # create threshold on first call
+    nb_cells = cols*rows
+    epsilon = epsilon/nb_cells
+    if not hasattr(detect_with_adaptive_threshold_and_grid, "thresholds"):
+        detect_with_adaptive_threshold_and_grid.thresholds = [
+            15 for _ in range(nb_cells)]
+    if init_thresholds is not None:
+        if len(init_thresholds) != cols*rows:
+            print("Init thresholds should be a list of size cols*rows")
+            return
+        detect_with_adaptive_threshold_and_grid.thresholds = init_thresholds
+
+    # use detect function to get the keypoints
+    nb_cols_per_cell = img.shape[1] // cols
+    nb_rows_per_cell = img.shape[0] // rows
+    nb_keypoints_per_cell = nb_keypoints // nb_cells
+    patches = extract_patches(img, nb_rows_per_cell, nb_cols_per_cell)
+    
+    print(nb_cols_per_cell, nb_rows_per_cell, patches.shape, img.shape)
+    
+    detect_with_adaptive_threshold_and_grid.nb_keypoints_per_cell = []
+    detect_with_adaptive_threshold_and_grid.keypoints_per_cell = []
+
+    for i, patch in enumerate(patches):
+        keypoints = detect(
+            patch, detect_with_adaptive_threshold_and_grid.thresholds[i],
+            N=N, step=step)
+    
+        # adapt the threshold in function of the number of keypoints
+        nb_keypoints_in_cell = keypoints.shape[0]
+        detect_with_adaptive_threshold_and_grid.nb_keypoints_per_cell.append(
+            nb_keypoints_in_cell)
+        detect_with_adaptive_threshold_and_grid.keypoints_per_cell.append(
+            keypoints)
+        if nb_keypoints_in_cell > nb_keypoints_per_cell + epsilon:
+            change = detect_with_adaptive_threshold_and_grid.thresholds[i]*percentage
+            if change < 1:
+                change = 1
+            else:
+                change = math.floor(change)
+            detect_with_adaptive_threshold_and_grid.thresholds[i] += change
+        elif nb_keypoints_in_cell < nb_keypoints_per_cell - epsilon:
+            change = detect_with_adaptive_threshold_and_grid.thresholds[i]*percentage
+            if change < 1:
+                change = 1
+            else:
+                change = math.floor(change)
+            detect_with_adaptive_threshold_and_grid.thresholds[i] -= change
+
+        patch_x_pos = i//cols
+        patch_y_pos = i-patch_x_pos*cols
+        offset_pos = np.array([patch_y_pos*nb_cols_per_cell, patch_x_pos*nb_rows_per_cell])
+        print(offset_pos)
+        keypoints = np.array([kp+offset_pos for kp in keypoints])
+        
+        if 'keypoints_per_cell' not in locals():
+            keypoints_per_cell = keypoints
+        else:
+            keypoints_per_cell = np.concatenate([keypoints_per_cell, keypoints])
+
+    return keypoints_per_cell, patches
+
 
 def detect_with_adaptive_threshold(
         img, nb_keypoints, N=12, step=3, epsilon=50,
@@ -64,9 +164,10 @@ def detect_with_adaptive_threshold(
             [Number of keypoints, x, y]
     """
     # create threshold on first call
-    if not hasattr(detect_with_adaptive_threshold, "threshold") or\
-            init_threshold is not None:
+    if not hasattr(detect_with_adaptive_threshold, "threshold"):
         detect_with_adaptive_threshold.threshold = 15
+    if init_threshold is not None:
+        detect_with_adaptive_threshold.threshold = init_threshold
 
     # use detect function to get the keypoints
     keypoints = detect(img, detect_with_adaptive_threshold.threshold,
