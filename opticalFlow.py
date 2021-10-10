7144e98d6263a83493e5f01810112fd1f6ec77c1
Apiquet <anthony.piquet2@gmail.com>
2021-10-10 17:33:51 +0200

    [DEV][FLOW] Add args to draw function

--- opticalFlow.py
+++ opticalFlow.py
@@ -7,6 +7,7 @@ Algorithm to detect keypoints
 """
 
 import cv2
+from PIL import Image
 import numpy as np
 import math
 from matplotlib import pyplot as plt
@@ -17,7 +18,7 @@ def compute():
     return 0
 
 
-def draw(frame, keypoints, flow):
+def draw(frame, keypoints, flow, draw_kp_with_zero_flow=False, factor=1):
     """
     Function to draw keypoints on an image with optical flow
 
@@ -30,8 +31,10 @@ def draw(frame, keypoints, flow):
     """
     img = frame.copy()
     for point in keypoints:
-        x_flow = int(flow[1][point[0], point[1]])
-        y_flow = int(flow[0][point[0], point[1]])
+        x_flow = int(flow[1][point[0], point[1]]*factor)
+        y_flow = int(flow[0][point[0], point[1]]*factor)
+        if x_flow == 0 and y_flow == 0 and not draw_kp_with_zero_flow:
+            continue
         end_flow = (point[1]-x_flow, point[0]-y_flow)
         cv2.circle(img, (point[1], point[0]), 2, (0, 255, 255), 4)
         cv2.line(img, (point[1], point[0]), end_flow, (255, 0, 0),
